c6b397768b4b1a1e47989e2ab8caf32cbe53b434
Shubham Kumar Gupta <shubhamkumargupta1012@gmail.com>
2024-10-14 22:57:14 +0530

    Stop tracking build directory

--- backend/build/utils/jwt.js
+++
@@ -1,8 +0,0 @@
-import jwt from 'jsonwebtoken';
-const generateToken = (userData) => {
-    if (!process.env.JWT_SECRET) {
-        throw new Error('JWT_SECRET is not defined in environment variables');
-    }
-    return jwt.sign(userData, process.env.JWT_SECRET, { expiresIn: '1h' });
-};
-export default generateToken;
