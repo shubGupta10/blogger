d5c48a4c7aa8800e6eccafa7225e73a26be202ce
Shubham Kumar Gupta <shubhamkumargupta1012@gmail.com>
2024-10-18 19:20:32 +0530

    Fixes for production in backend

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,25 +1,25 @@
 import { ApolloServer } from '@apollo/server';
-import {expressMiddleware } from "@apollo/server/express4"
-import cors from 'cors'
-import express from 'express'
-import {ApolloServerPluginDrainHttpServer} from '@apollo/server/plugin/drainHttpServer'
-import http from 'http'
+import { expressMiddleware } from "@apollo/server/express4";
+import cors from 'cors';
+import express from 'express';
+import { ApolloServerPluginDrainHttpServer } from '@apollo/server/plugin/drainHttpServer';
+import http from 'http';
 import mergedResolvers from './resolvers/mergedResolvers.js';
 import mergedTypeDefs from './typeDefs/mergedTypeDefs.js';
 import connect from './DbConfig/Connect.js';
 import authMiddleware from './Middlewares/authMiddlewares.js';
-import dotenv from 'dotenv'
+import dotenv from 'dotenv';
 
-dotenv.config()
-
+dotenv.config();
 
 interface MyContext {
     user?: any; 
     req: express.Request;
     res: express.Response;
 }
-const app = express()
-const httpServer = http.createServer(app)
+
+const app = express();
+const httpServer = http.createServer(app);
 
 const server = new ApolloServer<MyContext>({
     typeDefs: mergedTypeDefs,
@@ -28,28 +28,23 @@ const server = new ApolloServer<MyContext>({
     plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
 });
 
-
 await server.start();
 
-
-app.use("/", cors<cors.CorsRequest>({
+app.use(cors({
     origin: process.env.NEXT_PUBLIC_FRONTEND_URL,
     credentials: true
-}),
-express.json(),
-(req, res, next) => {
-    console.log(req.headers.cookie); 
-    next();
-},
-expressMiddleware(server, {
-    context: async ({req, res}) => {
+}));
+app.use(express.json());
+
+app.use(expressMiddleware(server, {
+    context: async ({ req, res }) => {
         const user = await authMiddleware(req, res);
         console.log("ye mera index.ts wala", user); 
-        return {req, res, user} as MyContext;
+        return { req, res, user } as MyContext;
     }
 }));
 
-
-await new Promise<void>((resolve) => httpServer.listen({ port: 4000 }, resolve));
+const PORT = process.env.PORT || 4000; 
+await new Promise<void>((resolve) => httpServer.listen({ port: PORT }, resolve));
 await connect();
-console.log(`🚀 Server ready at http://localhost:4000/`);
+console.log(`🚀 Server ready at http://localhost:${PORT}/`);
