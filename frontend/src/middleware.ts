d23bd7d76246dcacda450e83f6696a0712678c71
Shubham Kumar Gupta <shubhamkumargupta1012@gmail.com>
2024-10-19 16:55:50 +0530

    fixes in middleware

--- frontend/src/middleware.ts
+++ frontend/src/middleware.ts
@@ -1,32 +1,9 @@
 import { NextRequest, NextResponse } from "next/server";
 
 export function middleware(request: NextRequest) {
-    const token = request.cookies.get('token')?.value;
-    const { pathname } = request.nextUrl;
-
-    const publicPages = ['/', '/Auth/login', '/Auth/signUp'];
-
-    if (!token && !publicPages.includes(pathname)) {
-        return NextResponse.redirect(new URL('/Auth/login', request.url));
-    }
-
-    if (token && publicPages.includes(pathname)) {
-        return NextResponse.redirect(new URL('/pages/Dashboard', request.url));
-    }
-
-    return NextResponse.next();
+  return NextResponse.next();
 }
 
 export const config = {
-    matcher: [
-        '/',
-        '/Auth/login',
-        '/Auth/signUp',
-        '/pages/blogs/:id',
-        '/pages/createBlog',
-        '/pages/Dashboard',
-        '/pages/EditBlogs/:id',
-        '/pages/settings',
-        '/pages/userProfile/:id'
-    ],
+  matcher: [], 
 };
