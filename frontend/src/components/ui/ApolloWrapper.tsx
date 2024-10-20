9e1feb378a1652222bb5c27a495df5b3c6d513b7
Shubham Kumar Gupta <shubhamkumargupta1012@gmail.com>
2024-10-20 15:36:47 +0530

    fixes for token

--- frontend/src/components/ui/ApolloWrapper.tsx
+++ frontend/src/components/ui/ApolloWrapper.tsx
@@ -1,29 +1,16 @@
-import { ApolloClient, InMemoryCache, ApolloProvider, HttpLink, ApolloLink } from '@apollo/client';
-import { setContext } from '@apollo/client/link/context';
+import { ApolloClient, InMemoryCache, ApolloProvider, HttpLink } from '@apollo/client';
 import { ReactNode } from 'react';
 
 const httpLink = new HttpLink({
     uri: process.env.NEXT_PUBLIC_BACKEND_URL,
     credentials: 'include',
-});
-
-const authLink = setContext((_, { headers }) => {
-    // Get the authentication token from local storage if it exists
-    let token;
-    if (typeof window !== 'undefined') {
-        token = localStorage.getItem('token');
-    }
-    // Return the headers to the context so httpLink can read them
-    return {
-        headers: {
-            ...headers,
-            authorization: token ? `Bearer ${token}` : "",
-        }
-    }
+    headers: {
+        Authorization: typeof window !== 'undefined' ? `Bearer ${localStorage.getItem('token') || ''}` : '',
+    },
 });
 
 const client = new ApolloClient({
-    link: ApolloLink.from([authLink, httpLink]),
+    link: httpLink,
     cache: new InMemoryCache(),
 });
 
